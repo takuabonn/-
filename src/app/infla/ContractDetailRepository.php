6e788f0af2070208e6ccc452cf3e91118da571c2
takuabonn <k.takumi19980322@gmail.com>
2022-11-23 17:56:07 +0900

    契約内容リポジトリ作成

---
+++ src/app/infla/ContractDetailRepository.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\infla;
+
+use App\domains\repositories\IFContractDetailRepository;
+
+class ContractDetailRepository implements IFContractDetailRepository
+{
+    private $contractDetailList = [
+        [
+            'contract_line_id' => 1,
+            'payment_type' => 1,
+            'billing_deadline_type' => 1,
+            'plan_type' => 1,
+            'family_discount_condition' => 1,
+            'wifi_discount_condition' => 1,
+            'call_option' => 0,
+        ],
+        [
+            'contract_line_id' => 4,
+            'payment_type' => 1,
+            'billing_deadline_type' => 1,
+            'plan_type' => 1,
+            'family_discount_condition' => 1,
+            'wifi_discount_condition' => 1,
+            'call_option' => 0,
+        ]
+
+    ];
+    public function findByContractLineId($contract_line_id)
+    {
+        $keyIndex = array_search($contract_line_id, array_column($this->contractDetailList, 'contract_line_id'));
+        return $this->contractDetailList[$keyIndex];
+    }
+}

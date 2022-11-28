04fecd5716213ce42723a17f3907b4019dcd98c9
takuabonn <k.takumi19980322@gmail.com>
2022-11-28 22:02:00 +0900

    光契約の有無の判定の修正

--- src/app/infla/ContractDeviceRepository.php
+++ src/app/infla/ContractDeviceRepository.php
@@ -17,18 +17,11 @@ class ContractDeviceRepository implements IFContractDeviceRepository
 
     public function existsDeviceOnDeviceType($device_type, $contractor_id)
     {
-        $devices = ContractDevice::where('contractor_id', $contractor_id)->where('device_type', $device_type)->get()->toArray();
-
-        return count($devices) > 0;
+        return ContractDevice::where('contractor_id', $contractor_id)->where('device_type', $device_type)->count() > 0;
     }
 
     public function existsFamilyDeviceOnDeviceType($device_type, $contractor_ids)
     {
-        foreach ($contractor_ids as $contractor_id) {
-            if ($this->existsDeviceOnDeviceType($device_type, $contractor_id)) {
-                return true;
-            }
-        }
-        return false;
+        return ContractDevice::whereIn('contractor_id', $contractor_ids)->where('device_type', $device_type)->count() > 0;
     }
 }
